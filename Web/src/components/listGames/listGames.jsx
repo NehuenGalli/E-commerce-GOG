56ef632217f92a4797bf31dec2054b544011c1f6
Gonzalo Barreto <gonzalobarreto1911@gmail.com>
2025-05-15 21:41:36 -0300

    componente para mostrar games de tags, search, etc

---
+++ Web/src/components/listGames/listGames.jsx
@@ -0,0 +1,30 @@
+import GameExtraInfo from "./gameExtraInfo";
+import "./listGames.css";
+
+const ListGames = ({ games, nameTag }) => {
+  return (
+    <>
+      <div className="container mt-5">
+        <h1>TAG: {nameTag}</h1>
+
+        {games.map((game) => (
+          <div className="card mb-3 mx-5">
+            <div className="row g-0">
+              <div className="col-md-4">
+                <img
+                  src={game.mainImage}
+                  className="img-fluid rounded-start object-fit-cover"
+                  alt="..."
+                />
+              </div>
+
+              <GameExtraInfo game={game}></GameExtraInfo>
+            </div>
+          </div>
+        ))}
+      </div>
+    </>
+  );
+};
+
+export default ListGames;
--- Web/src/pages/tagGames.jsx
+++ Web/src/pages/tagGames.jsx
@@ -1,4 +1,5 @@
-import ListTagGames from "../components/listTagGames/listTagGames";
+import ListGames from "../components/listGames/listGames";
+import Paginacion from "../components/pagination/paginacion";
 import { useEffect, useState } from "react";
 import { useParams } from "react-router";
 import axios from "axios";
@@ -13,21 +14,25 @@ const TagGames = () => {
     amountOfElements: 0,
     amountOfPages: 0,
   });
-  //   const [currentPage, setCurrentPage] = useState(1);
+
   useEffect(() => {
     axios
       .get(`${API.BASE_URL}/tags/${tagId}?page=${1}`)
       .then((response) => setgames(response.data));
   }, [tagId]);
-  //   const primerPagina = () => setCurrentPage(1);
-  //   const anteriorPagina = () => setCurrentPage((prev) => Math.max(prev - 1, 1));
-  //   const siguientePagina = () =>
-  //     setCurrentPage((prev) => Math.min(prev + 1, games.amountOfPages));
-  //   const ultimaPagina = () => setCurrentPage(games.amountOfPages);
+
+  const elemento = games?.list?.[0]?.tags?.find((item) => item.id === tagId);
+
   return (
     <>
       <NavBar isLoggedIn={!!localStorage.getItem(API.TOKEN_KEY)} />
-      <ListTagGames games={games.list} />
+      <ListGames games={games.list} nameTag={elemento?.name} />
+      {/*       
+      <Paginacion
+        currentPage={currentPage}
+        totalPages={games.list.amountOfPages}
+        onPageChange={setCurrentPage}
+      /> */}
     </>
   );
 };
