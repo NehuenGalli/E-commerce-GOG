8abc5b142830d514316396dcede6e6d72f734237
Nehuen Gallitelli <nehuengallijans@gmail.com>
2025-05-29 22:28:16 -0300

    Modifico css cart

--- Web/src/components/cart/cartWithItems.jsx
+++ Web/src/components/cart/cartWithItems.jsx
@@ -10,25 +10,23 @@ const CartWithItems = ({ items, onRemove }) => {
   return (
     <>
       <div className="container">
-        <div className="row g-3 mt-2">
+        <div className="row g-3 mt-2 ">
           <div className="cart-with-items rounded p-4 col-12 col-lg-8">
             {items.map((game) => (
-              <div key={game.id} className="card my-3 border-0">
-                <div className="row align-items-center">
+              <div key={game.id} className="cart-item card my-3 border-0">
+                <div className="cart-item-row d-flex align-items-center">
                   <img
-                    className="col-12 col-md-6 "
+                    className="cart-item-image"
                     src={game.mainImage}
                     alt={game.name}
                   />
-
-                  <h3 className="col-12 col-md-3 text-center d-flex justify-content-center ">
+                  <div className="cart-item-name flex-grow-1">
                     {game.name}
-                  </h3>
-                  <div className="col-8 col-md-2 cart-price d-flex justify-content-center">
-                    USD {game.price.amount.toFixed(2)}
                   </div>
-
-                  <div className="col-2 col-md-1 d-flex justify-content-center">
+                  <div className="cart-item-actions d-flex align-items-center">
+                    <div className="cart-price me-2">
+                      USD {game.price.amount.toFixed(2)}
+                    </div>
                     <button
                       className="remove-item-btn"
                       onClick={() => onRemove(game.id)}
