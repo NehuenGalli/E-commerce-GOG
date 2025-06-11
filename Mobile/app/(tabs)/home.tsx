67e6fa81a812abf0430b2960674169acdab81478
Gonzalo Barreto <gonzalobarreto1911@gmail.com>
2025-06-11 02:29:01 -0300

    probando cosas

---
+++ Mobile/app/(tabs)/home.tsx
@@ -0,0 +1,13 @@
+import { View, Text } from "react-native"; // Also, import Text from react-native
+
+const Home = () => {
+  return (
+    <>
+      <View>
+        <Text>Home aaaaaaaaaaaaaaaaaa</Text>
+      </View>
+    </>
+  );
+};
+
+export default Home;
