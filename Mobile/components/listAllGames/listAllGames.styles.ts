181cec0f559da9ab194eb7d75741d7c4209b0b26
Alan Tubio <alannahuelmiya2@hotmail.com>
2025-06-15 20:06:13 -0300

    preparando para pull

--- Mobile/components/listAllGames/listAllGames.styles.ts
+++ Mobile/components/listAllGames/listAllGames.styles.ts
@@ -3,7 +3,6 @@ import { StyleSheet } from "react-native";
 export const styles = StyleSheet.create({
   list: {
     display: "flex",
-    justifyContent: "center",
     alignItems: "center",
   },
 
